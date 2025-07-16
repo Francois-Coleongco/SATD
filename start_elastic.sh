15397330b4b9ca9c4f8916f4502ebffbcd6bbba5
Francois Coleongco <christien.coleongco@gmail.com>
2025-07-16 16:56:12 -0600

    almost MVP :) i forgot i left gateway

--- start_elastic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# reference: https://www.elastic.co/docs/solutions/search/run-elasticsearch-locally
-# note that you must configure your elastic search to be secure, this is just for local development.
-
-curl -fsSL https://elastic.co/start-local | sh
