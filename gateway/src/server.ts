c57083ba0cb54bc0ac3a04ec9d2c219c75cb8cfe
Francois Coleongco <christien.coleongco@gmail.com>
2025-07-17 17:22:06 -0600

    node server with TLS and authentication + docker managed postgres B) hehehehhehehe now for linking with a frontend and calling elastic

---
+++ gateway/src/server.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import server from './app'
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+	console.log(`IT'S ALIVE ON https://localhost:${PORT}`)
+})
